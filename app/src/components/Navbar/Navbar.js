0a1287c3ffb266489ca907cd83ce344e2c022fd8
Mihir <mihirsingh@berkeley.edu>
2024-10-19 18:42:58 -0700

    Filled in Navbar.js

---
+++ app/src/components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+//import React
+// Import Link to navigate between pages
+// Import the CSS file for styling
+
+const Navbar = () => {
+    return (
+     // fill in the rest here!
+     // add Home and NotHome, including links (after routing established)
+};
+  
+  // Always export your components so they can be imported in other files
