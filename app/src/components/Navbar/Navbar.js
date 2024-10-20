560134c0400b222900054016850fe7d43923f179
Mihir <mihirsingh@berkeley.edu>
2024-10-20 12:41:09 -0700

    Created Navbar component and added it to App.js

--- app/src/components/Navbar/Navbar.js
+++ app/src/components/Navbar/Navbar.js
@@ -1,11 +1,20 @@
-//import React
-// Import Link to navigate between pages
-// Import the CSS file for styling
+import React from "react";
+import {Link} from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
     return (
-     // fill in the rest here!
+     <nav class = "navbar">
+        <div class = "header">
+            My website
+        </div>
+        <div class = "links">
+            <a> Home </a>
+            <a> Not Home </a>
+        </div>
+     </nav>
      // add Home and NotHome, including links (after routing established)
-};
+)};
   
   // Always export your components so they can be imported in other files
+  export default Navbar;
