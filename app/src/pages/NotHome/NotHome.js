497038edad0448accbae1024de11758f3854bf53
Mihir <mihirsingh@berkeley.edu>
2024-10-24 14:01:53 -0700

    implemented counter

--- app/src/pages/NotHome/NotHome.js
+++ app/src/pages/NotHome/NotHome.js
@@ -1,8 +1,19 @@
 import React from "react";
+import {useState, useEffect} from 'react';
 
 const NotHome = () => {
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+
+    }, [count]); //Re-run effect every time count changes.
+
     return (
-     <h2>Not Home Page</h2>
+        <div>
+            <h2>Not Home Page</h2>
+            <p>Count: {count}</p>
+            <button onClick = {() => setCount(count + 1)}>Click me</button>
+        </div>    
 )};
 
 export default NotHome;
