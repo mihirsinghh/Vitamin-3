26d28c2ecbf68cde1fb06fa8e607a24dfb2a9c12
Mihir <mihirsingh@berkeley.edu>
2024-10-24 14:24:48 -0700

    implemented useEffect and useState hooks in NotHome.js

--- app/src/pages/NotHome/NotHome.js
+++ app/src/pages/NotHome/NotHome.js
@@ -3,9 +3,15 @@ import {useState, useEffect} from 'react';
 
 const NotHome = () => {
     const [count, setCount] = useState(0);
+    const [message, setMessage] = useState("");
 
     useEffect(() => {
-
+        if (count < 0) {
+            setCount(0);
+        }
+        if (count > 5) {
+            setMessage("You passed 5!");
+        }
     }, [count]); //Re-run effect every time count changes.
 
     return (
@@ -13,6 +19,7 @@ const NotHome = () => {
             <h2>Not Home Page</h2>
             <p>Count: {count}</p>
             <button onClick = {() => setCount(count + 1)}>Click me</button>
+            <p>{message}</p>
         </div>    
 )};
 
